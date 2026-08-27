6819ab16ce147e89058fef3735db11a1341c8315
Fonyi Mate Sandor <128932963+fonyimatesandor@users.noreply.github.com>
2026-08-27 22:56:53 +0800

    Create rHMM_MVO backtesting setup

---
+++ scripts/generate_mc_backtests_rHMM_MVO_static_skewed_t.py
@@ -0,0 +1,87 @@
+import pickle as pkl
+import json
+
+from scripts.mc_backtest_setup import (
+    config,
+    simulators,
+    asset_prices_comparison,
+    rHMM_MVO_models,
+    rHMM_MVO_comparison_models,
+)
+
+with open("data/hmm_model/hmm_model.pkl", "rb") as f:
+    hmm_model = pkl.load(f)
+
+with open("data/hmm_model/hmm_feature_extractor.pkl", "rb") as f:
+    feature_extractor = pkl.load(f)
+
+simulator_name = "static_skewed_t"
+
+with open(f"data/synthetic/{simulator_name}_simulated_data.pkl", "rb") as f:
+    simulator, simulated_paths = pkl.load(f)
+
+simulated_prices = simulated_paths["simulated_prices"]
+simulated_factors = simulated_paths["simulated_factors"]
+
+for model in rHMM_MVO_models:
+    print(
+        f"Running MC backtest for model {model['name']} on simulator {simulator_name}..."
+    )
+    testing_model = model["class"](
+        assets=asset_prices_comparison,
+        **{
+            **model["kwargs"],
+            "hmm_model": hmm_model,
+            "hmm_feature_extractor": feature_extractor,
+        },
+    )
+    testing_model.run_backtest()
+    mc_results = testing_model.run_MC_backtest(
+        precomputed_prices=simulated_prices,
+        num_simulations=config["monte_carlo"]["num_simulations"],
+        workers=-1,
+        batch_size=config["monte_carlo"]["chunk_size"],
+    )
+    with open(
+        f"data/rhmm_mvo_processed/{model['name']}_{simulator_name}_MC_backtest_results.pkl",
+        "wb",
+    ) as f:
+        pkl.dump(mc_results, f)
+
+
+for model in rHMM_MVO_comparison_models:
+
+    print(
+        f"Running MC backtest for model {model['name']} on simulator {simulator_name}..."
+    )
+
+    testing_model = model["class"](assets=asset_prices_comparison, **model["kwargs"])
+    testing_model.run_backtest()
+
+    mc_results = testing_model.run_MC_backtest(
+        precomputed_prices=simulated_prices,
+        num_simulations=config["monte_carlo"]["num_simulations"],
+        workers=-1,
+        batch_size=config["monte_carlo"]["chunk_size"],
+    )
+
+    with open(
+        f"data/rhmm_mvo_processed/{model['name']}_{simulator_name}_MC_backtest_results.pkl",
+        "wb",
+    ) as f:
+        pkl.dump(mc_results, f)
+
+
+json.dump(
+    {
+        "models_tested": [model["name"] for model in rHMM_MVO_models]
+        + [model["name"] for model in rHMM_MVO_comparison_models],
+        "simulators_tested": [simulator_name],
+        "config": config,
+    },
+    open(
+        f"data/rhmm_mvo_processed/mc_backtest_summary_rHMM_MVO_{simulator_name}.json",
+        "w",
+    ),
+    indent=4,
+)
